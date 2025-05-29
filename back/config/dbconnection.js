2f9b17267139d7826d222eee28d1e8888dde6b1d
Shruti Khannukar <109957253+shrutikhannukar@users.noreply.github.com>
2025-05-30 01:32:33 +0530

    Update dbconnection.js

--- back/config/dbconnection.js
+++ back/config/dbconnection.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     // const db = process.env.MONGO_URI;
     // await mongoose.connect("mongodb+srv://nandini:nandini@cluster0.wixe7rn.mongodb.net/parlour?retryWrites=true&w=majority&appName=Cluster0");
     await mongoose.connect(
-      "mongodb+srv://shrutikhannukar1020:parlour12345@parlour.k2xnzfr.mongodb.net/beautyParlourApp"
+      "mongodb+srv://shrutikhannukar1020:parlour12345@parlour.k2xnzfr.mongodb.net/beautyParlourAppe"
     );
     console.log("MongoDB connected...");
   } catch (err) {
